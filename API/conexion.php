b663a91b6b60b8f232bd466a765ffcb7dcac106e
mati <sepulvedamati2@gmail.com>
2025-12-02 09:10:23 -0300

    conexion render

--- API/conexion.php
+++ API/conexion.php
@@ -1,19 +1,26 @@
 <?php
 header("Content-Type: application/json");
 require_once 'middleware_log.php';
-$host = 'localhost';
-$port = '5433';
-$dbname = 'captacionCliente';
-$user = 'postgres';
-$password = '159753';
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+$connectionString = "postgresql://captacioncliente_user:W8zeu4sTlpatQsWJtHxzl6qK3zHaxtl6@dpg-d4mvl73uibrs738ujdkg-a/captacioncliente";
+
+$parts = parse_url($connectionString);
+
+$host = $parts['host'];
+$port = $parts['port'] ?? 5432;
+$user = $parts['user'];
+$pass = $parts['pass'];
+$db   = ltrim($parts['path'], '/');
 
 try {
-    $conn = new PDO($dsn, $user, $password);
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
-    echo json_encode(['success' => false, 'error' => 'Error al conectar a la base de datos: ' . $e->getMessage()]);
+    echo json_encode([
+        'success' => false,
+        'error' => 'Error al conectar a la base de datos: ' . $e->getMessage()
+    ]);
     exit;
 }
 ?>
